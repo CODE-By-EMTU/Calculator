7512b9a7819ed9ea982381e5140ce0aa8a9f8528
Tanjil_Mahmud_Emtu <142157754+CODE-By-EMTU@users.noreply.github.com>
2023-11-24 20:11:30 +0600

    script.js

--- Calculetor/script.js
+++ Calculetor/script.js
@@ -1,13 +1,24 @@
-function DeleteMe() {
-    document.getElementById("myresult").value = "";
-  }
 
-  function caluolet(NewV) {
-    document.getElementById("myresult").value += NewV;
-  }
-
-  function ans() {
-    var a = document.getElementById("myresult").value;
-    var b = eval(a);
-    document.getElementById("myresult").value = b;
-  }
+    function DeleteMe() {
+      document.getElementById("myresult").value = "";
+    }
+  
+    function caluolet(NewV) {
+      document.getElementById("myresult").value += NewV;
+    }
+  
+    function ans() {
+      var input = document.getElementById("myresult").value;
+  
+      try {
+        var result = evaluateExpression(input);
+        document.getElementById("myresult").value = result;
+      } catch (error) {
+        document.getElementById("myresult").value = "Math Error";
+      }
+    }
+  
+    function evaluateExpression(expression) {
+      return new Function('return ' + expression)();
+    }
+  
